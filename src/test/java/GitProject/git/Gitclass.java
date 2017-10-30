51849493da7b10fb3755b69e646cd0b2b9ff9ddd
22706 <Itcinfotech-Github>
2017-10-30 16:20:51 +0530

    Changed the file

--- src/test/java/GitProject/git/Gitclass.java
+++ src/test/java/GitProject/git/Gitclass.java
@@ -5,7 +5,8 @@ public class Gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		System.out.println("git class 1");
+
 		System.out.println("git class 1");
 	}
 
