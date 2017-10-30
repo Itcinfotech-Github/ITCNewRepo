5b0b4f271ad5ee9165b48f8043d5be4e28786a94
Satyajit Samal <Itcinfotech-Github>
2017-10-30 15:20:17 +0530

    first commit

---
+++ src/test/java/GitProject/git/Gitclass.java
@@ -0,0 +1,12 @@
+package GitProject.git;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("git class");
+	}
+
+}
